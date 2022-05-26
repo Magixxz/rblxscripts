82d714a1d5fb80b8f4c15ef53940a40411dc41f7
Magixxz <77512851+Magixxz@users.noreply.github.com>
2022-05-26 00:04:40 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,14 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Cheesy", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Magixxz Testing", HidePremium = true, SaveConfig = false, ConfigFolder = "OrionTest"})
+OrionLib:MakeNotification({
+	Name = "Welcome!",
+	Content = "Testing!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
 local Tab = Window:MakeTab({
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
-OrionLib:MakeNotification({
-	Name = "Testing!",
-	Content = "Testing Purposes... Magixxz#3038",
-	Image = "rbxassetid://4483345998",
-	Time = 5
-})
 OrionLib:Init()
-
