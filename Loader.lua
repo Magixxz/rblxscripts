d547ee66617c81b7b9ad6220c6c1627969d9518a
Magixxz <77512851+Magixxz@users.noreply.github.com>
2022-06-26 14:36:56 -0400

    Fixes

--- Loader.lua
+++ Loader.lua
@@ -3,24 +3,25 @@ CoreGui:SetCore("SendNotification", {
  Title = "Embotic Script Hub",
  Text = "Loading...",
 Duration = 5;
-}) 
+})
 if not game:IsLoaded() then
     repeat wait() until game:IsLoaded()
 end
 if game.PlaceId == 8365830854 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Magixxz/rblxscripts/main/EraofEncantoGUI.lua"))()
 CoreGui:SetCore("SendNotification", {
  Title = "Era of Encanto GUI",
  Text = "Script Loading",
 Duration = 5;
 })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Magixxz/rblxscripts/main/EraofEncantoGUI.lua"))()
+end
 if game.PlaceId == 6755746130 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Magixxz/rblxscripts/main/FruitJuiceTycoonGUI.lua"))()
 CoreGui:SetCore("SendNotification", {
  Title = "Fruit Juice Tycoon GUI",
  Text = "Script Loading",
 Duration = 5;
 })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Magixxz/rblxscripts/main/FruitJuiceTycoonGUI.lua"))()
 else
     CoreGui:SetCore("SendNotification", {
  Title = "Game not supported!",
